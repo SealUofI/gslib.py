747d8184e7bcf1b1d9b8af4243c9b95c8b5b6d48
Thilina Rathnayake <thilinarmtb@gmail.com>
2018-03-11 13:47:50 -0500

    Update src/gslib_wrapper.pyx

--- src/gslib_wrapper.pyx
+++ src/gslib_wrapper.pyx
@@ -1,4 +1,8 @@
 from gslib cimport comm, comm_init, comm_free
+from gslib cimport gs_data, gs_setup
+from gslib cimport gs_auto, gs_pairwise, gs_crystal_router, gs_all_reduce
+from gslib cimport gs_add, gs_mul, gs_min, gs_max, gs_bpr
+
 import logging
 
 logger = logging.getLogger('gslib_wrapper')
@@ -9,15 +13,16 @@ fh.setLevel(logging.DEBUG)
 
 logger.addHandler(fh)
 
-cdef class PyBase(object):
-    cdef void_ptr ptr
-
 cdef class PyComm(object):
     cdef comm c
+    cdef gs_data *g
 
     def __cinit__(self, int ec):
         comm_init(&self.c, ec)
         logger.debug('Created comm object.')
+        cdef long long id = self.c.id
+        g = gs_setup(&id, 1, &self.c, 0, gs_auto, 1)
+        logger.debug('Setup gs library.')
 
     def __dealloc__(self):
         comm_free(&self.c)
